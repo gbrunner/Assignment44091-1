1603c4c35517e18b5f969a51d18cc00324457faa
erheinrichs <97911839+erheinrichs@users.noreply.github.com>
2022-02-16 00:23:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,111 @@
-require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
-        var map = new Map({
-          basemap: "topo-vector"
-        });
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 4,
-          center: [15, 65] // longitude, latitude
-        });
+    require([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/Camera",
+      "esri/widgets/Home",
+      "dojo/domReady!"
+    ], function(WebScene, SceneView, Camera, Home) {
+
+    
+      /*var map = new Map({
+        basemap: "streets",
+        ground: "world-elevation"
+      });*/
+      var scene = new WebScene({
+        portalItem:{
+         id:"8046207c1c214b5587230f5e5f8efc77" 
+        }
       });
+      
+      var camera = new Camera({
+        position: [
+          -71.0542, // lon
+          42.3647, // lat
+          8000// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      })
+      
+      var camera2 = new Camera({
+        position: {
+          x: -71.0765,
+          y: 42.3388,
+          z: 2500
+        },
+        tilt: -35,
+        heading: 0
+      })
+      var camera3 = new Camera({
+        position: [
+          -71.0589, // lon
+          42.3601, // lat
+          2500// elevation in meters
+        ],
+        tilt:0,
+        heading: 0
+      });
+
+      var view = new SceneView({
+        container: "viewDiv",
+        map: scene,
+        viewingMode:"global",
+        camera: camera,
+        environment: {
+            lighting: {
+              date: new Date(),
+              directShadowsEnabled: true,
+              // don't update the view time when user pans.
+              // The clock widget drives the time
+              cameraTrackingEnabled: false
+            }
+        },
+    });
+    
+    var homeBtn = new Home({
+        view: view
+      });
+
+      // Add the home button to the top left corner of the view
+    view.ui.add(homeBtn, "top-left");
+    
+    [v1, v2, v3].forEach(function(button) {
+      button.style.display = 'flex';
+      view.ui.add(button, 'top-right');
+    });
+    
+    v2.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        target:camera2
+      });
+    });
+    
+    v1.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position:{
+          x:-71.0542,
+          y:42.3647,
+          z: 2500
+        },
+        tilt: 35,
+        heading: 0
+      });
+    });
+      v3.addEventListener('click', function() {
+      // reuse the default camera position already established in the homeBtn
+      view.goTo({
+        position: {
+          x: -70.93699101091352,
+          y: 42.36715064583551, 
+          z: 250
+        },
+        tilt: 90,
+        heading: -90
+      });
+    });
+
+
+    });
